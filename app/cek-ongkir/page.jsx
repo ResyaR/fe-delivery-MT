fe2a4a57993a850549e8cced89330a7d8ba1d05a
ResyaR <resyarizqi387@gmail.com>
2025-09-10 14:06:25 +0700

    refactor: move cek-ongkir page to client component

--- app/cek-ongkir/page.jsx
+++ app/cek-ongkir/page.jsx
@@ -1,24 +1,9 @@
-"use client"
+import dynamic from 'next/dynamic'
 
-import { useSearchParams } from 'next/navigation'
-import Header from '../../components/cek-ongkir/Header.jsx'
-import OngkirForm from '../../components/cek-ongkir/OngkirForm.jsx'
-import Footer from '../../components/cek-ongkir/Footer.jsx'
+const CekOngkirClient = dynamic(() => import('../../components/cek-ongkir/CekOngkirClient'), {
+  ssr: false
+})
 
 export default function CekOngkirPage() {
-  const searchParams = useSearchParams();
-  const type = searchParams.get('type');
-  // @ts-ignore
-  return (
-    <div className="flex flex-col bg-white">
-      <div 
-        className="flex flex-col items-start self-stretch h-[1074px]"
-        style={{ background: "linear-gradient(180deg, #C3CFE2, #FCFCFC)" }}
-      >
-        <Header />
-        <OngkirForm type={type || 'pengiriman-instan'} />
-        <Footer />
-      </div>
-    </div>
-  )
+  return <CekOngkirClient />
 }
